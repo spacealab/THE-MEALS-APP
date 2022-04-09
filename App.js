a2bfd9b80a6409d6f00df609fb466ed3c5e67979
Ali Alipour <42216151+spacealab@users.noreply.github.com>
2022-04-09 12:30:56 +0430

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+import { enableScreen } from 'react-native-screen';
+
+import MealsNavigator from './navigation/MealsNavigator';
+
+enableScreen();
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  // if (!fontLoaded) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={fetchFonts}
+  //       onFinish={() => setFontLoaded(true)}
+  //     />
+  //   );
+  // }
+
+  return <MealsNavigator />;
+}
---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data'
+import Colors from '../constants/Colors';
+
+const CategoryMealScreen = props => {
+  const catId = props.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+
+  return (
+    <View style={styles.screen}>
+      <Text>{selectedCategory.title}</Text>
+      <Button title="Go to Details" onPress={() => {
+          props.navigation.navigate({
+              routeName: 'MealDetail'
+          });
+      }} />
+      <Button title="Go Back" onPress={() => {
+          props.navigation.pop();
+      }} />
+    </View>
+  );
+};
+
+CategoryMealScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+  };
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+export default CategoryMealScreen;
